d3f511309cf58cbd3b2733c11efd68a1b3110e2e
brisiacorona <brisiacorona@gmail.com>
2014-09-10 12:02:53 -0500

    Paso18

--- roman.py
+++ roman.py
@@ -1,20 +1,18 @@
-#Numeros romanos
-
 import re
 
 roman_numeral_map = (('M',  1000),
-					('CM', 900),
-					('D',  500),
-					('CD', 400),
-					('C',  100),
-					('XC', 90),
-					('L',  50),
-					('XL', 40),
-					('X',  10),
-					('IX', 9),
-					('V',  5),
-					('IV', 4),
-					('I',  1)) 
+                     ('CM', 900),
+                     ('D',  500),
+                     ('CD', 400),
+                     ('C',  100),
+                     ('XC', 90),
+                     ('L',  50),
+                     ('XL', 40),
+                     ('X',  10),
+                     ('IX', 9),
+                     ('V',  5),
+                     ('IV', 4),
+                     ('I',  1))
 
 roman_numeral_pattern = re.compile('''
     ^                   # beginning of string
@@ -29,47 +27,45 @@ roman_numeral_pattern = re.compile('''
     ''', re.VERBOSE)
 
 
-
 def to_roman(n):
 
-	if not (0 < n < 4000): 
-		raise OutOfRangeError('number out of range (must be less than 1...3999)') 
+    if not (0 < n < 4000):
+        raise OutOfRangeError(
+            'number out of range (must be less than 1...3999)')
 
-	if not isinstance(n, int):                                          
-		raise NotIntegerError('non-integers can not be converted')
+    if not isinstance(n, int):
+        raise NotIntegerError('non-integers can not be converted')
 
-	result = ''
-	for numeral, integer in roman_numeral_map:
-		while n >= integer:                     
-			result += numeral
-			n -= integer
-	return result
+    result = ''
+    for numeral, integer in roman_numeral_map:
+        while n >= integer:
+            result += numeral
+            n -= integer
+    return result
 
 
 def from_roman(numero_romano):
-	'''convert Roman numeral to integer'''
-	if not roman_numeral_pattern.search(numero_romano):
-		raise InvalidRomanNumeralError('Invalid Roman numeral: {0}'.format(numero_romano))
+    '''convert Roman numeral to integer'''
+    if not roman_numeral_pattern.search(numero_romano):
+        raise InvalidRomanNumeralError(
+            'Invalid Roman numeral: {0}'.format(numero_romano))
 
-	result = 0
-	index = 0
-	for numeral, integer in roman_numeral_map:
-		while numero_romano[index:index+len(numeral)] == numeral:  
-			result += integer
-			index += len(numeral)
-		    print('found', numeral, 'of length', len(numeral), ', adding', integer)
-	return result
-
-
+    result = 0
+    index = 0
+    for numeral, integer in roman_numeral_map:
+        while numero_romano[index:index + len(numeral)] == numeral:
+            result += integer
+            index += len(numeral)
+    return result
 
 
-class OutOfRangeError(ValueError): pass
-
-class NotIntegerError(ValueError): pass
-
-class InvalidRomanNumeralError(ValueError): pass
+class OutOfRangeError(ValueError):
+    pass
 
 
+class NotIntegerError(ValueError):
+    pass
 
-    
-	
+
+class InvalidRomanNumeralError(ValueError):
+    pass
